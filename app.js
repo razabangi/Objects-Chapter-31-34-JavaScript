adf8e2416720d8d7184757894a273c998f52eb4c
Muhammadrazabangi09 <muhammad.raza@cooperativecomputing.com>
2021-11-20 23:31:36 +0500

    object complete

--- app.js
+++ app.js
@@ -102,6 +102,56 @@
 // document.write(`Later date: ${date}`);
 
 // Answer no 9:
+// let currentDate = new Date();
+// let ramazanDate = new Date('04/12/2021');
+// let diffTime = Math.abs(ramazanDate - currentDate);
+// let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+// document.write(`${diffDays} days have passed since 1st ramazan, 2021`);
 
+// Answer no 10:
+// let firstDateOfDec = new Date('01/01/2015');
+// let lastDateOfDec = new Date('12/05/2015 22:50:16');
+// let diffTime = Math.abs(lastDateOfDec - firstDateOfDec);
+// let diffSeconds = Math.ceil(diffTime / (1000 * 60));
+// document.write(`On reference date ${lastDateOfDec}, ${diffSeconds} seconds had passed since beginning of 2015`);
+
+// Answer no 11:
+// let currentDate = new Date();
+// let date = new Date();
+// currentDate.setHours( currentDate.getHours() + 1 );
+// document.write(`current date: ${date}<br> 1 hour ago, it was ${currentDate}`);
+
+// Answer no 12:
+// let currentYear = new Date();
+// let pastCenturyYear = new Date();
+// let pastYear = pastCenturyYear.getFullYear() - 100;
+// pastCenturyYear.setFullYear(pastYear);
+// document.write(`current date: ${currentYear}<br> 100 years back, it was ${pastCenturyYear}`);
+
+// Answer no 13:
+// let age = +prompt("Enter your age");
+// let year = new Date().getFullYear();
+// let calculateYear = year - age;
+// document.write(`Your age is ${age}<br> Your birth year is ${calculateYear}`);
+
+// Answer no 14:
+// document.write(`<h1>K-Electric Bill</h1>`);
+// let customerName = prompt("Enter your name");
+// let monthsArr = ['january','febraury','march','april','may','june','july','august','september','october','november','december'];
+// let date = new Date();
+// let currentMonth = monthsArr[date.getMonth()];
+// let numOfUnits = Math.floor(+prompt("Enter number of units"));
+// let chargePerUnit = Math.floor(+prompt("Enter per unit charger"));
+// let latePayableCharge = Math.floor(+prompt("Late payable charge"));
+// let netAmountPayable = numOfUnits * chargePerUnit;
+// let grossAmountPayable = netAmountPayable + latePayableCharge;
+
+// document.write(`Customer Name: ${customerName}<br>`);
+// document.write(`Month: ${currentMonth}<br>`);
+// document.write(`Number of units: ${numOfUnits}<br>`);
+// document.write(`Charge per unit: ${chargePerUnit}<br><br>`);
+// document.write(`Net Amount Payable (within Due Date): ${netAmountPayable}<br>`);
+// document.write(`Late payment surcharge: ${latePayableCharge}<br>`);
+// document.write(`Gross Amount Payable (after Due Date): ${grossAmountPayable}<br>`);
 
 // End of Chapter 31 to 34
